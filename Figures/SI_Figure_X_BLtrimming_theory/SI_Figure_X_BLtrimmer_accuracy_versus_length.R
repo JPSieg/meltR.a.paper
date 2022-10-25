87d6863449c3d181df8461f43e0e600a83d1598f
JPSieg <jpsieg225@gmail.com>
2022-10-25 11:38:30 -0400

    Update figure 2 and move the BLtrimmer theory to a SI figure

--- Figures/SI_Figure_X_BLtrimming_theory/SI_Figure_X_BLtrimmer_accuracy_versus_length.R
+++ Figures/SI_Figure_X_BLtrimming_theory/SI_Figure_X_BLtrimmer_accuracy_versus_length.R
@@ -3,11 +3,13 @@ library(tidyverse)
 library(cowplot)
 devtools::load_all()
 
-meltR.A(df.absorbance %>% filter(Experiment == "Helix A"),
+fit = meltR.A(df.absorbance %>% filter(Experiment == "Helix A"),
         blank = 1,
         NucAcid = c("RNA", "CGAAAGGU", "ACCUUUCG"),
         Mmodel = "Heteroduplex.2State",
         fitTs = c(25, 80))
+
+Trim = BLTrimmer(fit)
 
 dH.range = c(-80, -60)
 Tm.range = c(40, 55)
@@ -31,6 +33,12 @@ f = function(H, S, Temperature, Ct){
   model <- Mmodels$Heteroduplex.2State(K = K, Ct = Ct)
   return(model)
 }
+
+TmModel = function(H, S, lnCt){
+  ((0.0019872/H)*lnCt) + ((S - 0.0019872*log(4))/H)
+}
+
+calcS = function(H, Tm, Ct){ (H/(273.15 + Tm)) + (0.0019872*log(4/Ct)) }
 
 ####Fit a real but ideal melting curve####
 
@@ -70,14 +78,13 @@ df.ranges = data.frame(v.lows,
 df.ranges$Width = df.ranges$v.highs - df.ranges$v.lows
 df.ranges$Baseline.length = df.ranges$v.highs - high
 
-dH.known = 64.76
-Tm.known = 46.4
-  
 mDS = c()
 bDS = c()
 mSS = c()
 bSS = c()
 dH = c()
+dS = c()
+dG = c()
 Tm = c()
 
 for (i in 1:nrow(df.ranges)){
@@ -92,8 +99,11 @@ for (i in 1:nrow(df.ranges)){
     bDS[i] = coef(fit$Method.1.fit[[1]])[4]
     mSS[i] = coef(fit$Method.1.fit[[1]])[5]
     bSS[i] = coef(fit$Method.1.fit[[1]])[6]
-    dH[i] = coef(fit$Method.1.fit[[1]])[1]
-    Tm[i] = coef(fit$Method.1.fit[[1]])[2]
+    dH[i] = fit$Method.1.indvfits$dH[1]
+    dS[i] = fit$Method.1.indvfits$dS[1]
+    dG[i] = fit$Method.1.indvfits$dG[1]
+    Tm[i] = fit$Method.1.indvfits$Tm[1]
+    
     
   }, error = function(e){})
   
@@ -104,8 +114,16 @@ df.ranges$bDS = bDS
 df.ranges$mSS = mSS
 df.ranges$bSS = bSS
 df.ranges$dH = dH
+df.ranges$dS = dS
+df.ranges$dG = dG
 df.ranges$Tm = Tm
 
+calcTM = function(H, S, Ct){(H/((S/1000) - 0.0019872*log(4/Ct))) - 273.15}
+
+dH.known = -68.40
+dS.known = -187.89
+dG.known = -10.13
+Tm.known = calcTM(dH.known, dS.known, fit$Method.1.indvfits$Ct[1])
 
 top = ggplot() +
   geom_abline(data = df.ranges, mapping = aes(slope = mDS, intercept = bDS, color = Width)) +
@@ -118,7 +136,7 @@ top = ggplot() +
   xlab("Temperature (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-middle = ggplot(df.ranges) +
+dH = ggplot(df.ranges) +
   geom_hline(yintercept = dH.known) +
   geom_point(mapping = aes(x = Baseline.length, y = dH, color = Baseline.length)) +
   theme_classic() +
@@ -129,7 +147,33 @@ middle = ggplot(df.ranges) +
   ylab("\u0394H\u00B0 (kcal/mol)") +
   theme(axis.text = element_text(color = "black"))
 
-bottom = ggplot(df.ranges) +
+dS.range = c(-187.89-20, -187.89+20)
+
+dS = ggplot(df.ranges) +
+  geom_hline(yintercept = dS.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dS, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dS.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+dG.range = c(-10.13-2, -10.13+2)
+
+dG = ggplot(df.ranges) +
+  geom_hline(yintercept = dG.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dG, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dG.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+Tm = ggplot(df.ranges) +
   geom_hline(yintercept = Tm.known) +
   geom_point(mapping = aes(x = Baseline.length, y = Tm, color = Baseline.length)) +
   theme_classic() +
@@ -140,10 +184,10 @@ bottom = ggplot(df.ranges) +
   ylab("Tm (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-Ideal = plot_grid(top, middle, bottom,
+Ideal = plot_grid(top, dH, dS, dG, Tm,
                       align = "v",
                       ncol = 1,
-                  rel_heights = c(1.8, 1, 1))
+                  rel_heights = c(2, 1, 1, 1, 1))
 
 ####Make modeled data####
 
@@ -158,23 +202,20 @@ ggplot(df, aes(x = Temperature, y = Absorbance)) +
 
 df = df %>% filter(Sample == 4)
 
-?meltR.A
-
 fit = meltR.A(df,
         NucAcid = c("RNA", "CGAAAGGU", "ACCUUUCG"),
         Mmodel = "Heteroduplex.2State",
         methods = c(TRUE, FALSE, FALSE))
 
 
-fit$Summary$S[1]
-fit$Summary$H[1]
+fit$Summary$dS[1]
+fit$Summary$dH[1]
 
-df$f = f(fit$Summary$H[1], fit$Summary$S[1]/1000, df$Temperature, fit$Method.1.indvfits$Ct)
+df$f = f(dH.known, dS.known/1000, df$Temperature, fit$Method.1.indvfits$Ct)
 
 plot(df$Temperature, df$f)
 
-
-Tm = fit$Summary$H[1]/((fit$Summary$S[1]/1000) - 0.00198720425864083*log(4/))
+Tm = dH.known/((dS.known/1000) - 0.00198720425864083*log(4/fit$Method.1.indvfits$Ct))
 
 mDS = df.ranges$mDS[15]
 bDS = df.ranges$bDS[15]
@@ -187,7 +228,7 @@ Tm-273.15
 
 df.model = df
 
-df.model$Absorbance = (10^5)*((mDS*df.model$Temperature + bDS)*Ct*df.model$f + (mSS*df.model$Temperature + bSS)*Ct*(1 - df.model$f)) + rnorm(nrow(df.model), 0, 0.0001)
+df.model$Absorbance = (10^5)*((mDS*df.model$Temperature + bDS)*Ct*df.model$f + (mSS*df.model$Temperature + bSS)*Ct*(1 - df.model$f)) + rnorm(nrow(df.model), 0, 0.00005)
 
 plot(df.model$Temperature, df.model$Absorbance)
 
@@ -218,47 +259,61 @@ bDS = c()
 mSS = c()
 bSS = c()
 dH = c()
+dS = c()
+dG = c()
 Tm = c()
 
-for (i in 1:nrow(df.model.ranges)){
+for (i in 1:nrow(df.ranges)){
   tryCatch({
     fit = meltR.A(df.model,
                   NucAcid = c("RNA", "CGAAAGGU", "ACCUUUCG"),
                   Mmodel = "Heteroduplex.2State",
                   methods = c(TRUE, FALSE, FALSE),
-                  fitTs = c(df.model.ranges$v.lows[i], df.model.ranges$v.highs[i]),
+                  fitTs = c(df.ranges$v.lows[i], df.ranges$v.highs[i]),
                   Silent = TRUE)
     mDS[i] = coef(fit$Method.1.fit[[1]])[3]
     bDS[i] = coef(fit$Method.1.fit[[1]])[4]
     mSS[i] = coef(fit$Method.1.fit[[1]])[5]
     bSS[i] = coef(fit$Method.1.fit[[1]])[6]
-    dH[i] = coef(fit$Method.1.fit[[1]])[1]
-    Tm[i] = coef(fit$Method.1.fit[[1]])[2]
+    dH[i] = fit$Method.1.indvfits$dH[1]
+    dS[i] = fit$Method.1.indvfits$dS[1]
+    dG[i] = fit$Method.1.indvfits$dG[1]
+    Tm[i] = fit$Method.1.indvfits$Tm[1]
+    
     
   }, error = function(e){})
   
 }
 
-df.model.ranges$mDS = mDS
-df.model.ranges$bDS = bDS
-df.model.ranges$mSS = mSS
-df.model.ranges$bSS = bSS
-df.model.ranges$dH = dH
-df.model.ranges$Tm = Tm
+df.ranges$mDS = mDS
+df.ranges$bDS = bDS
+df.ranges$mSS = mSS
+df.ranges$bSS = bSS
+df.ranges$dH = dH
+df.ranges$dS = dS
+df.ranges$dG = dG
+df.ranges$Tm = Tm
 
+calcTM = function(H, S, Ct){(H/((S/1000) - 0.0019872*log(4/Ct))) - 273.15}
+
+dH.known = -68.40
+dS.known = -187.89
+dG.known = -10.13
+Tm.known = calcTM(dH.known, dS.known, fit$Method.1.indvfits$Ct[1])
 
 top = ggplot() +
-  geom_abline(data = df.model.ranges, mapping = aes(slope = mDS, intercept = bDS, color = Width)) +
-  geom_abline(data = df.model.ranges, mapping = aes(slope = mSS, intercept = bSS, color = Width)) +
+  geom_abline(data = df.ranges, mapping = aes(slope = mDS, intercept = bDS, color = Width)) +
+  geom_abline(data = df.ranges, mapping = aes(slope = mSS, intercept = bSS, color = Width)) +
   geom_point(data = df.model, mapping = aes(x = Temperature, y = Absorbance),
              color = "black", alpha = 0.2) +
   theme_classic() +
   scale_color_viridis_b() +
-  theme(legend.position = "none") +
+  theme(legend.position = "none")  +
   xlab("Temperature (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-middle = ggplot(df.model.ranges) +
+dH = ggplot(df.ranges) +
+  geom_hline(yintercept = dH.known) +
   geom_point(mapping = aes(x = Baseline.length, y = dH, color = Baseline.length)) +
   theme_classic() +
   scale_y_continuous(limits = dH.range) +
@@ -268,7 +323,34 @@ middle = ggplot(df.model.ranges) +
   ylab("\u0394H\u00B0 (kcal/mol)") +
   theme(axis.text = element_text(color = "black"))
 
-bottom = ggplot(df.model.ranges) +
+dS.range = c(-187.89-20, -187.89+20)
+
+dS = ggplot(df.ranges) +
+  geom_hline(yintercept = dS.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dS, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dS.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+dG.range = c(-10.13-2, -10.13+2)
+
+dG = ggplot(df.ranges) +
+  geom_hline(yintercept = dG.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dG, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dG.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+Tm = ggplot(df.ranges) +
+  geom_hline(yintercept = Tm.known) +
   geom_point(mapping = aes(x = Baseline.length, y = Tm, color = Baseline.length)) +
   theme_classic() +
   scale_y_continuous(limits = Tm.range) +
@@ -278,10 +360,11 @@ bottom = ggplot(df.model.ranges) +
   ylab("Tm (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-Model = plot_grid(top, middle, bottom,
-          align = "v",
-          ncol = 1,
-          rel_heights = c(1.8, 1, 1))
+Model = plot_grid(top, dH, dS, dG, Tm,
+                  align = "v",
+                  ncol = 1,
+                  rel_heights = c(2, 1, 1, 1, 1))
+
 
 ####Fit a real but non-ideal melting curve####
 
@@ -300,7 +383,7 @@ fit = meltR.A(df,
               methods = c(TRUE, FALSE, FALSE))
 
 
-df$f = f(fit$Summary$H[1], fit$Summary$S[1]/1000, df$Temperature, fit$Method.1.indvfits$Ct)
+df$f = f(fit$Summary$dH[1], fit$Summary$dS[1]/1000, df$Temperature, fit$Method.1.indvfits$Ct)
 
 plot(df$Temperature, df$f)
 
@@ -327,6 +410,8 @@ bDS = c()
 mSS = c()
 bSS = c()
 dH = c()
+dS = c()
+dG = c()
 Tm = c()
 
 for (i in 1:nrow(df.ranges)){
@@ -341,8 +426,11 @@ for (i in 1:nrow(df.ranges)){
     bDS[i] = coef(fit$Method.1.fit[[1]])[4]
     mSS[i] = coef(fit$Method.1.fit[[1]])[5]
     bSS[i] = coef(fit$Method.1.fit[[1]])[6]
-    dH[i] = coef(fit$Method.1.fit[[1]])[1]
-    Tm[i] = coef(fit$Method.1.fit[[1]])[2]
+    dH[i] = fit$Method.1.indvfits$dH[1]
+    dS[i] = fit$Method.1.indvfits$dS[1]
+    dG[i] = fit$Method.1.indvfits$dG[1]
+    Tm[i] = fit$Method.1.indvfits$Tm[1]
+    
     
   }, error = function(e){})
   
@@ -353,8 +441,16 @@ df.ranges$bDS = bDS
 df.ranges$mSS = mSS
 df.ranges$bSS = bSS
 df.ranges$dH = dH
+df.ranges$dS = dS
+df.ranges$dG = dG
 df.ranges$Tm = Tm
 
+calcTM = function(H, S, Ct){(H/((S/1000) - 0.0019872*log(4/Ct))) - 273.15}
+
+dH.known = -68.40
+dS.known = -187.89
+dG.known = -10.13
+Tm.known = calcTM(dH.known, dS.known, fit$Method.1.indvfits$Ct[1])
 
 top = ggplot() +
   geom_abline(data = df.ranges, mapping = aes(slope = mDS, intercept = bDS, color = Width)) +
@@ -363,11 +459,12 @@ top = ggplot() +
              color = "black", alpha = 0.2) +
   theme_classic() +
   scale_color_viridis_b() +
-  theme(legend.position = "none") +
-  xlab("Temperature (\u00B0C)")  +
+  theme(legend.position = "none")  +
+  xlab("Temperature (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-middle = ggplot(df.ranges) +
+dH = ggplot(df.ranges) +
+  geom_hline(yintercept = dH.known) +
   geom_point(mapping = aes(x = Baseline.length, y = dH, color = Baseline.length)) +
   theme_classic() +
   scale_y_continuous(limits = dH.range) +
@@ -377,7 +474,34 @@ middle = ggplot(df.ranges) +
   ylab("\u0394H\u00B0 (kcal/mol)") +
   theme(axis.text = element_text(color = "black"))
 
-bottom = ggplot(df.ranges) +
+dS.range = c(-187.89-20, -187.89+20)
+
+dS = ggplot(df.ranges) +
+  geom_hline(yintercept = dS.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dS, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dS.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+dG.range = c(-10.13-2, -10.13+2)
+
+dG = ggplot(df.ranges) +
+  geom_hline(yintercept = dG.known) +
+  geom_point(mapping = aes(x = Baseline.length, y = dG, color = Baseline.length)) +
+  theme_classic() +
+  scale_y_continuous(limits = dG.range) +
+  scale_color_viridis_b() +
+  theme(legend.position = "none")  +
+  xlab("Baseline length (\u00B0C)") +
+  ylab("\u0394S\u00B0 (cal/mol/K)") +
+  theme(axis.text = element_text(color = "black"))
+
+Tm = ggplot(df.ranges) +
+  geom_hline(yintercept = Tm.known) +
   geom_point(mapping = aes(x = Baseline.length, y = Tm, color = Baseline.length)) +
   theme_classic() +
   scale_y_continuous(limits = Tm.range) +
@@ -387,15 +511,18 @@ bottom = ggplot(df.ranges) +
   ylab("Tm (\u00B0C)") +
   theme(axis.text = element_text(color = "black"))
 
-Non.ideal = plot_grid(top, middle, bottom,
-          align = "v",
-          ncol = 1,
-          rel_heights = c(1.8, 1, 1))
+Non.ideal = plot_grid(top, dH, dS, dG, Tm,
+                  align = "v",
+                  ncol = 1,
+                  rel_heights = c(2, 1, 1, 1, 1))
+
 
 ####Consolidate plot####
 
 P = plot_grid(Model, Ideal, Non.ideal, nrow = 1,
               labels = c("A", "B", "C"), label_size = 16)
 
-ggsave("Figures/Figure_2_Baseline_trimmer/Figure_2.svg", P,
-       width = 5.3, height = 2.3, units = "in", scale = 2)
+list.files("Figures/SI_Figure_X_BLtrimming_theory")
+
+ggsave("Figures/SI_Figure_X_BLtrimming_theory/SI_Figure_X_BLtrimming_theory.svg", P,
+       width = 5.3, height = 4.5, units = "in", scale = 2)
