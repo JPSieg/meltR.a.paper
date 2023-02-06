abd2c3cd79d37c6f4bb56a510beb750006a9576a
JPSieg <jpsieg225@gmail.com>
2023-02-06 12:56:37 -0500

    Make Figure S4E in terms of dG error instread of %dG error

--- Figures/SI_Figure_4_BLtrimmer_modeled_data_Meltwin_MeltR_agrrement_with_NN/SI_Figure_X_NN_agreement.R
+++ Figures/SI_Figure_4_BLtrimmer_modeled_data_Meltwin_MeltR_agrrement_with_NN/SI_Figure_X_NN_agreement.R
@@ -24,9 +24,9 @@ for (i in 1:length(unique(df.MeltR$Helix))){
   df.MeltR$Known.dS[which(df.MeltR$Helix == helix)] = df.known$dS[which(df.known$Helix == helix)]
 }
 
-df.MeltR$error.dG = (df.MeltR$dG - df.MeltR$Known.dG)/df.MeltR$Known.dG
-df.MeltR$error.dH = (df.MeltR$dH - df.MeltR$Known.dH)/df.MeltR$Known.dH
-df.MeltR$error.dS = (df.MeltR$dS - 1000*df.MeltR$Known.dS)/(1000*df.MeltR$Known.dS)
+df.MeltR$error.dG = (df.MeltR$dG - df.MeltR$Known.dG)
+df.MeltR$error.dH = (df.MeltR$dH - df.MeltR$Known.dH)
+df.MeltR$error.dS = (df.MeltR$dS - 1000*df.MeltR$Known.dS)
 
 
 df1 = df.MeltR %>%
@@ -49,9 +49,9 @@ for (i in 1:length(unique(df.Meltwin$Helix))){
   df.Meltwin$Known.dS[which(df.Meltwin$Helix == helix)] = df.known$dS[which(df.known$Helix == helix)]
 }
 
-df.Meltwin$error.dG = (df.Meltwin$dG - df.Meltwin$Known.dG)/df.Meltwin$Known.dG
-df.Meltwin$error.dH = (df.Meltwin$dH - df.Meltwin$Known.dH)/df.Meltwin$Known.dH
-df.Meltwin$error.dS = (df.Meltwin$dS - 1000*df.Meltwin$Known.dS)/(1000*df.Meltwin$Known.dS)
+df.Meltwin$error.dG = (df.Meltwin$dG - df.Meltwin$Known.dG)
+df.Meltwin$error.dH = (df.Meltwin$dH - df.Meltwin$Known.dH)
+df.Meltwin$error.dS = (df.Meltwin$dS - 1000*df.Meltwin$Known.dS)
 
 
 df2 = df.Meltwin %>%
@@ -83,24 +83,29 @@ for (i in 1:nrow(df)){
 
 df$Mmodel = Mmodel
 
-View(df)
+#View(df)
 
-ggplot(df, aes(x = Program, y = 100*error.dG)) +
+df.mean = df %>% group_by(Program) %>% summarise(mean =mean(abs(error.dG), na.rm = T))
+
+ggplot() +
   geom_hline(yintercept =  0) +
-  geom_line(mapping = aes(group = Helix, color = Mmodel)) + 
-  stat_compare_means(label.x = 2, label.y = 10) +
+  geom_text(data = df.mean, mapping = aes(x = Program, y = 1.1, label = round(mean, 2))) +
+  geom_errorbar(data = df.mean, mapping = aes(x = Program, ymin = 0-mean, ymax = 0 + mean), size = 2)+ 
+  
+  geom_line(data = df, mapping = aes(x = Program, y = error.dG, group = Helix, color = Mmodel)) + 
+  stat_compare_means(data = df, mapping = aes(x = Program, y = error.dG), label.x = 2, label.y = -1.1) +
   scale_color_manual(values = viridis::viridis(4)) +
   #scale_y_continuous(limits = c(-10, 10), breaks = c(-10, -7.5, -5, -2.5, 0, 2.5, 5, 7.5, 10)) +
   #stat_compare_means(comparisons = l.comp) +
-  geom_beeswarm(mapping = aes(color = Mmodel, shape = Mmodel), size = 3) +
+  geom_beeswarm(data = df, mapping = aes(x = Program, y = error.dG, color = Mmodel, shape = Mmodel), size = 3) +
   theme_classic() +
-  scale_y_continuous(breaks = c(-50, -25, -10, -5, 0, 5,  10), limits = c(-50, 10)) +
+  scale_y_continuous(breaks = c(-1.25, -1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75,  1, 1.25), limits = c(-1.25, 1.25)) +
   theme(axis.text.x = element_text(color = "black",
                                     angle = 45,
                                    hjust = 1),
         axis.text.y = element_text(color = "black")) +
   xlab("") +
-  ylab("%error \u0394G\u00B037")
+  ylab("\u0394G\u00B037")
 
 list.files("Figures")
 
